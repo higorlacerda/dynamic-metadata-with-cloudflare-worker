26781fcc3d9483ecd90e7df8cf455874271c974e
Higor Lacerda <49889268+higorlacerda@users.noreply.github.com>
2025-03-24 20:16:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,19 @@ export const config = {
   domainSource: "https://e3c412aa-5c74-4d02-b483-1802d4c8adc9.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/empresa/[^/]+",
-          metaDataEndpoint: "https://api.griiingo.com/rest/v1/companies_metadata?slug=eq.{slug}"
+					pattern: "/empresa/[^/]+",
+          metaDataEndpoint: "https://api.griiingo.com/rest/v1/companies_metadata?slug=eq.{slug}",
+					apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeXVoYWxzYXFpY2N2YXpjYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MTQyNTcsImV4cCI6MjAzNDQ5MDI1N30.QD_47Uq2OfmIXmNdz7mSTPO9z4qgRKpNbEPew2sb500"
       },
       {
           pattern: "/evento/[^/]+",
-          metaDataEndpoint: "https://api.griiingo.com/rest/v1/events_metadata?slug=eq.{slug}"
+          metaDataEndpoint: "https://api.griiingo.com/rest/v1/events_metadata?slug=eq.{slug}",
+					apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeXVoYWxzYXFpY2N2YXpjYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MTQyNTcsImV4cCI6MjAzNDQ5MDI1N30.QD_47Uq2OfmIXmNdz7mSTPO9z4qgRKpNbEPew2sb500"
       },
       {
           pattern: "/artigo/[^/]+",
-          metaDataEndpoint: "https://api.griiingo.com/rest/v1/article_metadata?slug=eq.{slug}"
+          metaDataEndpoint: "https://api.griiingo.com/rest/v1/article_metadata?slug=eq.{slug}",
+					apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeXVoYWxzYXFpY2N2YXpjYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MTQyNTcsImV4cCI6MjAzNDQ5MDI1N30.QD_47Uq2OfmIXmNdz7mSTPO9z4qgRKpNbEPew2sb500"
       }
       // Add more patterns and their metadata endpoints as needed
   ]
