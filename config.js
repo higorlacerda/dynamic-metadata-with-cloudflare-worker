e2a33065d908020c0653c89138aedfc475888fe4
Higor Lacerda <49889268+higorlacerda@users.noreply.github.com>
2025-03-24 21:46:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,17 +4,14 @@ export const config = {
       {
 	      pattern: "/empresa/[^/]+",
 	      metaDataEndpoint: "https://api.griiingo.com/rest/v1/companies_metadata?slug=eq.{company_url}",
-	      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeXVoYWxzYXFpY2N2YXpjYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MTQyNTcsImV4cCI6MjAzNDQ5MDI1N30.QD_47Uq2OfmIXmNdz7mSTPO9z4qgRKpNbEPew2sb500"
       },
       {
 	      pattern: "/evento/[^/]+",
 	      metaDataEndpoint: "https://api.griiingo.com/rest/v1/events_metadata?slug=eq.{slug}",
-	      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeXVoYWxzYXFpY2N2YXpjYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MTQyNTcsImV4cCI6MjAzNDQ5MDI1N30.QD_47Uq2OfmIXmNdz7mSTPO9z4qgRKpNbEPew2sb500"
       },
       {
 	      pattern: "/artigo/[^/]+",
 	      metaDataEndpoint: "https://api.griiingo.com/rest/v1/article_metadata?slug=eq.{slug}",
-	      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeXVoYWxzYXFpY2N2YXpjYmtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MTQyNTcsImV4cCI6MjAzNDQ5MDI1N30.QD_47Uq2OfmIXmNdz7mSTPO9z4qgRKpNbEPew2sb500"
       }
       // Add more patterns and their metadata endpoints as needed
   ]
